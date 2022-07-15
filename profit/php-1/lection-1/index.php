ea11170ecd97f81e99876027eacf0b33bdde3f09
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-07-15 09:14:35 +0700

    первый коммит

---
+++ profit/php-1/lection-1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lection 1</title>
+</head>
+<body>
+  <h1>Привет, PHP</h1>
+  <?php
+    $argc = 1;
+    $asd = '1 22 1 2';
+    var_dump(1 == 1.0);
+    var_dump(1 === 1.0);
+    var_dump('02' == 2);
+    var_dump('02' === 2);
+    var_dump('02' == '2');
+  ?>
+</body>
+</html>
